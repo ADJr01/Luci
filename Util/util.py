65a25e45b0804a14913109bc34958a7657a9ffb1
Rahat Adnan <sthep90@gmail.com>
2026-01-06 01:35:37 +0600

    solving embedding dimension issue

--- Util/util.py
+++ Util/util.py
@@ -1,2 +1,5 @@
-def get_embedding_dim_ollama(model):
-    pass
+from ollama import embed
+
+def get_embedding_dim_ollama(model:str):
+    result = embed(model=model,input="test query")
+    return len(result['embeddings'][0])
--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 from OllamaMem import Memory, OllamaMemoryBuilder as OLM
 import hashlib
+from Util.util import get_embedding_dim_ollama
 OLLAMA_MODEL = 'rnj-1:latest'
 EMBEDDING_MODEL = 'qwen3-embedding:0.6b'
-
+RERANKING_MODEL = 'dengcao/Qwen3-Reranker-4B:Q8_0'
 
 if __name__ == '__main__':
     mem = (OLM.OllamaMemoryBuilder()
               .collection_name("test_collection")
-              .persist_dir("./faiss_storage")
+              .persist_dir("./store")
+              .embedding_dims(dims=get_embedding_dim_ollama(EMBEDDING_MODEL))
               .ollama_model(OLLAMA_MODEL)
               .embedding_model(EMBEDDING_MODEL)
+              .re_ranking_model(RERANKING_MODEL)
               .max_tokens(3000)
               .temperature(0.1314)
               .build())
 
     mem.set_user_session("rahat.adnan")
     mem.add("I love teal color.",metadata={"feeling":"love","thing":"color"})
-    mem.add("I hate Sun",metadata={"feeling":"hate","thing":"sun"})
-    res = mem.search("thing i love")
-    print(res)
+    mem.add("I hate Cockroach",metadata={"feeling":"hate","thing":"sun"})
+    mem.add("I hate Sun",metadata={"feeling":"hate","thing":"Cockroach"})
+    mem.add("I hate Rats",metadata={"feeling":"hate","thing":"Rats"})
+
+    res = mem.search("things i like")
+    for i in res['results']:
+        if i['score']>0.6:
+            continue
+        print(i['memory'])
+    mem.end_user_session()
+
+
+
+
 
 
