d65d9d49cad365de1f78cf126d5e6d5a11f77ca9
Rahat Adnan <sthep90@gmail.com>
2026-02-08 23:44:54 +0600

    testing controller

--- controller/controller.py
+++ controller/controller.py
@@ -2,9 +2,12 @@ from ContextRetrievalStore import OllamaMemoryBuilder as OLM
 from Util.llm_util import get_embedding_dim_ollama
 from Util.util import (read_file,__std_out__)
 import controller.config as config
+
+# global log function
 def log(*args):
-      if config.MODE.lower() == 'dev':
-            print(__std_out__(*args))
+      if config.MODE.lower() != 'dev':
+            return
+      print(__std_out__(*args))
 
 class Controller(object):
 
